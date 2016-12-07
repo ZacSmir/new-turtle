b266020f56e36c5ff648077cffff1a75f1ac6ae3
ZacSmir <Zach@smirin.us>
2016-12-07 09:26:31 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ turnLeft(360/n)
 $("#clickdraw").click( function() {
   n = $("#draw").val()
 cat(n)
-star(6)
+star(n)
 })
 
 
 function star(x){
 for (i=0; i<x; i++){
-goForward(360/x)
-turnLeft(720/x)
-goForward(360/x)
-turnleft(720/x)
+goForward(360/n)
+turnLeft(720/n)
+goForward(360/n)
+turnRight(360/n)
 }  
 }
 
