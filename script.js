fd6039ac7bd3c53121fdf3a03455cf5cb9d55714
ZacSmir <Zach@smirin.us>
2016-12-09 08:51:03 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var n = $("#draw").val()
 
-function cat(n){
+function cat(n,x){
 for (i=0; i<n; i++){
-goForward(360/n)
+goForward(x/n)
 turnLeft(360/n)
 }
 }
@@ -24,6 +24,7 @@ cat(n)
 //}  
 //}
 
-
-
- 
+cat(360, 360)
+cat(360,180)
+cat(360, 90)
+cat(360, 45)
